c200814a0a8fbe5b693998c892ac2fd3b4333119
PPProsenjite <prosenjitpappu03455@gmial.com>
2024-12-11 18:34:51 +0600

    series 1+2+3+.......+n

---
+++ series.java
@@ -0,0 +1,35 @@
+class series{
+   
+    public long seriesNumber(int n){
+        
+         int i = 1;
+         int sum = 0;
+         // series 1 + 2 + 3 + ........+ n
+         //using loop check one by one and add 
+         while(i <= n){
+            sum = sum + i;
+            i++;
+        }
+       // conditon of series 1 + 2 + 3 + ........+ n
+       // sum = n * (n+1) /2;
+        
+        
+        return sum;
+        
+    }
+    
+ }
+ 
+ public class Main
+ {
+     public static void main(String[] args) {
+        
+         int n = 20;
+         
+         series obj = new series();
+         
+         System.out.println(n + " Sum of the series is : "+ obj.seriesNumber(n));
+         
+        
+     }
+ }
